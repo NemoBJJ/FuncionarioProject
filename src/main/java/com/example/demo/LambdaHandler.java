8ccc243fb08958460595e028f869441d77a33c8e
NemoBJJ <engnfaraujo@gmail.com>
2025-02-02 22:10:54 +0000

    Adicionando LambdaHandler e ajustes no POM.xml

---
+++ src/main/java/com/example/demo/LambdaHandler.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        return "Lambda Funcionando! Input recebido: " + input;
+    }
+}
